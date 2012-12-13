263ad6f37a7f04e2ab0b8fee356ab47b3154f53c
Matthias Villiger <mvi@bsiag.com>
2012-12-13 11:55:18 +0100

    Not necessary anymore that a text provider service is available in a scout project.
    If not present a new one is created.

--- org.eclipse.scout.sdk.saml.importer/src/org/eclipse/scout/sdk/saml/importer/operation/nls/TranslationElementImportOperation.java
+++ org.eclipse.scout.sdk.saml.importer/src/org/eclipse/scout/sdk/saml/importer/operation/nls/TranslationElementImportOperation.java
@@ -14,19 +14,29 @@ import java.util.Locale;
 
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.scout.nls.sdk.internal.NlsCore;
 import org.eclipse.scout.nls.sdk.internal.jdt.INlsFolder;
 import org.eclipse.scout.nls.sdk.internal.jdt.NlsFolder;
 import org.eclipse.scout.nls.sdk.model.util.Language;
 import org.eclipse.scout.nls.sdk.model.workspace.NlsEntry;
 import org.eclipse.scout.nls.sdk.model.workspace.project.INlsProject;
+import org.eclipse.scout.nls.sdk.services.model.ws.project.ServiceNlsProjectProvider;
+import org.eclipse.scout.nls.sdk.services.operation.CreateServiceNlsProjectOperation;
 import org.eclipse.scout.nls.sdk.simple.model.ws.INlsType;
 import org.eclipse.scout.nls.sdk.simple.model.ws.project.SimpleNlsProject;
 import org.eclipse.scout.nls.sdk.simple.ui.dialog.language.TranslationFileNewModel;
 import org.eclipse.scout.nls.sdk.ui.action.INewLanguageContext;
 import org.eclipse.scout.saml.saml.LanguageAttribute;
 import org.eclipse.scout.saml.saml.TranslationElement;
+import org.eclipse.scout.sdk.RuntimeClasses;
 import org.eclipse.scout.sdk.saml.importer.internal.SamlImporterActivator;
 import org.eclipse.scout.sdk.saml.importer.operation.AbstractSamlElementImportOperation;
+import org.eclipse.scout.sdk.util.SdkProperties;
+import org.eclipse.scout.sdk.util.type.ITypeFilter;
+import org.eclipse.scout.sdk.util.type.TypeFilters;
+import org.eclipse.scout.sdk.util.type.TypeUtility;
 
 /**
  * <h3>{@link TranslationElementImportOperation}</h3> ...
@@ -50,14 +60,66 @@ public class TranslationElementImportOperation extends AbstractSamlElementImport
     }
   }
 
+  private IType getCurrentTextService() throws JavaModelException {
+    IType[] services = ServiceNlsProjectProvider.getRegisteredTextProviderTypes();
+    ITypeFilter filter = TypeFilters.getClassesInProject(getCurrentScoutModule().getSharedBundle().getJavaProject());
+    for (IType type : services) {
+      if (filter.accept(type)) {
+        return type;
+      }
+    }
+    return null;
+  }
+
+  private static String getProjectSimpleName(String projectName) {
+    if (projectName == null) {
+      return null;
+    }
+    int pos = projectName.lastIndexOf('.') + 1;
+    int simpleNameLen = projectName.length() - pos;
+    if (pos > 0 && simpleNameLen > 0) {
+      String moduleSimpleName = projectName.substring(pos);
+      if (moduleSimpleName.length() > 1) {
+        return Character.toUpperCase(moduleSimpleName.charAt(0)) + moduleSimpleName.substring(1).toLowerCase();
+      }
+      else {
+        return moduleSimpleName.toUpperCase();
+      }
+    }
+    return null;
+  }
+
   @Override
   public void run() throws CoreException, IllegalArgumentException {
     String key = getTranslationElement().getName();
 
-    INlsProject nlsProject = getCurrentScoutModule().getNlsProject();
+    String projectName = getCurrentScoutModule().getProjectName();
+    IType curTxtSvc = getCurrentTextService();
+    if (!TypeUtility.exists(curTxtSvc)) {
+      String moduleSimpleName = getProjectSimpleName(projectName);
+      if (moduleSimpleName != null) {
+        CreateServiceNlsProjectOperation newTextServiceOp = new CreateServiceNlsProjectOperation();
+        newTextServiceOp.setBundle(getCurrentScoutModule().getSharedBundle());
+        newTextServiceOp.setLanguages(new String[]{null /* default language */}); // other languages will be added as needed.
+        newTextServiceOp.setServiceName(moduleSimpleName + SdkProperties.SUFFIX_TEXT_SERVICE);
+        newTextServiceOp.setSuperType(TypeUtility.getType(RuntimeClasses.AbstractDynamicNlsTextProviderService));
+        newTextServiceOp.setTranslationFilePrefix("Texts");
+        newTextServiceOp.setTranslationFolder("resources/texts");
+        newTextServiceOp.validate();
+        newTextServiceOp.run(getSamlContext().getMonitor(), getSamlContext().getWorkingCopyManager());
+        curTxtSvc = newTextServiceOp.getCreatedServiceType();
+      }
+      else {
+        throw new IllegalArgumentException("Invalid project name: '" + projectName + "'.");
+      }
+    }
+
+    INlsProject nlsProject = NlsCore.getNlsWorkspace().getNlsProject(new Object[]{curTxtSvc});
     if (nlsProject == null) {
-      throw new IllegalArgumentException("No NLS project could be found in module '" + getCurrentScoutModule().getProjectName() +
-          "'. Ensure a TextProviderService exists for this module.");
+      throw new IllegalArgumentException("No NLS project could be found in module '" + projectName + "'.");
+    }
+    if (nlsProject.getTranslationResource(Language.LANGUAGE_DEFAULT).isReadOnly()) {
+      throw new IllegalArgumentException("NLS project found for module '" + projectName + "' is readonly.");
     }
 
     INlsType txtProvSvc = null;
