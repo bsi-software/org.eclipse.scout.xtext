e7606ae3a31c2ba209aeb6751a69089b506e71c9
Matthias Villiger <mvi@bsiag.com>
2013-05-30 11:10:17 +0200

    organize imports AST improved to find more elements.

--- org.eclipse.scout.sdk.saml.importer/src/org/eclipse/scout/sdk/saml/importer/internal/jdt/imports/SamlOrganizeImportService.java
+++ org.eclipse.scout.sdk.saml.importer/src/org/eclipse/scout/sdk/saml/importer/internal/jdt/imports/SamlOrganizeImportService.java
@@ -30,9 +30,9 @@ public class SamlOrganizeImportService implements IOrganizeImportService {
   public void organize(ICompilationUnit cu, IProgressMonitor monitor) throws CoreException {
     @SuppressWarnings("deprecation")
     final ASTParser parser = ASTParser.newParser(AST.JLS3);
-    parser.setResolveBindings(false);
-    parser.setStatementsRecovery(false);
-    parser.setBindingsRecovery(false);
+    parser.setResolveBindings(true);
+    parser.setStatementsRecovery(true);
+    parser.setBindingsRecovery(true);
     parser.setSource(cu);
     CompilationUnit astRoot = (CompilationUnit) parser.createAST(null);
 
